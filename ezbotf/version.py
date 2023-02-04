b0959de03d5798a476076e9ba2ef76c620486d34
ftdot <ftdoot@gmail.com>
2023-02-05 00:07:08 +0200

    (Refactor, Version extended) General refactor
    Version extended to 1.0.0:
    modified:   ezbotf/version.py
    
    Refactor pyproject.toml, add information about project:
    modified:   pyproject.toml

--- ezbotf/version.py
+++ ezbotf/version.py
@@ -15,5 +15,5 @@ class VersionLabel:
 
 ezbotf_labels   = [VersionLabel.InDevelopment]
 
-ezbotf_version_string = '1.0.0.dev2'
+ezbotf_version_string = '1.0.0'
 ezbotf_version_string_full = ' '.join([ezbotf_version_string] + ezbotf_labels)
